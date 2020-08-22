7a8651d2bad7e7f43e3aad91cc2606146cc83708
minkyeong <alsrud713@gmail.com>
2020-08-22 10:36:35 +0900

    login-auth-added

--- photo/views.py
+++ photo/views.py
@@ -6,6 +6,12 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from django.shortcuts import redirect
 
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+@login_required
+
 
 def photo_list(request):
     photos = Photo.objects.all()
